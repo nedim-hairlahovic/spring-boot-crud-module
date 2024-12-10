f889accfd60b87441cb078d72ff0d80a3019fb94
Nedim Hairlahovic <nedim_hairlahovic@hotmail.de>
2024-12-10 20:59:26 +0100

    Add constructor to ResourceNotFoundException for custom messages

--- src/main/java/dev/nhairlahovic/crud/exception/ResourceNotFoundException.java
+++ src/main/java/dev/nhairlahovic/crud/exception/ResourceNotFoundException.java
@@ -2,6 +2,10 @@ package dev.nhairlahovic.crud.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
 
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
     public ResourceNotFoundException(String resourceType, Long id) {
         super(resourceType + " (ID: " + id + ") not found");
     }
